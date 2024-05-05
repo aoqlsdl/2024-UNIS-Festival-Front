1eaf53c637dde0f582ffd7ba053c7061dd92556b
aoqlsdl <hyeeev@gmail.com>
2024-05-05 21:31:36 +0900

    🌈ui: 상단배너 bg 임시 수정

--- src/components/UpperBanner/UpperBannerStyles.js
+++ src/components/UpperBanner/UpperBannerStyles.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	width: 100vw;
-	max-width: 23.438rem;
+	min-width: 23.438rem;
 	max-width: 49.125rem;
 	height: 300px;
-	background-color: #000;
+	background-color: #dedede;
 `;
