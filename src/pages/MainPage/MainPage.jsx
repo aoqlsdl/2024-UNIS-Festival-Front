e1f293e17ab86afdeb6ecdd9a21d8a91b671418c
aoqlsdl <hyeeev@gmail.com>
2024-05-05 21:31:58 +0900

    🌈ui: 메인페이지 컴포넌트 수정

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,20 +1,33 @@
 import UpperBanner from '../../components/UpperBanner/UpperBanner';
 import LowerBanner from '../../components/LowerBanner/LowerBanner';
 import * as s from './MainStyles';
-import CategoryButton from '../../components/CategoryButton/CategoryButton';
-import SurveyButton from '../../components/SurveyButton/SurveyButton';
-import BoothMap from '../../components/BoothMap/BoothMap';
+import MainButton from '../../components/MainButton/MainButton';
+import { useEffect, useState } from 'react';
+import { GetBriefReviewByTime } from '../../lib/api/review';
 
 const MainPage = () => {
+	const [reviews, setReviews] = useState([]);
+
+	// useEffect(() => {
+	// 	GetBriefReviewByTime().then(data => {
+	// 		console.log(data);
+	// 	});
+	// }, []);
 	return (
 		<>
 			<UpperBanner />
 			<s.Contents>
-				<CategoryButton />
-				<SurveyButton />
-				<BoothMap /> {/* BoothMap 클릭시 ImgPopUp 컴포넌트 나타남 */}
+				<s.Title>환영하오...</s.Title>
+				<MainButton title={'예약 주문'} desc={'기다리지 말고 픽업하세요!'} />
+				<MainButton title={'이벤트'} desc={'참여만 해도 반값할인'} />
+				<MainButton title={'오시는 길'} desc={'부스 위치 안내도'} />
 			</s.Contents>
-			<s.Contents></s.Contents>
+			<s.Contents>
+				<s.Title>벗들의 생생한 후기</s.Title>
+			</s.Contents>
+			<s.Contents>
+				<s.Title>어젯밤 구해주신 홍보물입니다</s.Title>
+			</s.Contents>
 			<LowerBanner />
 		</>
 	);
