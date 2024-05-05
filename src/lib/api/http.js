72a16b03fa5ad5d7d7dc38b63676aedc7fba982e
aoqlsdl <hyeeev@gmail.com>
2024-05-05 13:23:31 +0900

    ✨feat: 예약정보 api 연결

--- src/lib/api/http.js
+++ src/lib/api/http.js
@@ -4,5 +4,3 @@ export const http = axios.create({
 	baseURL: import.meta.env.VITE_APP_API_URL,
 	contentType: 'application/json; charset=utf-8;',
 });
-
-http.defaults.withCredentials = true;
--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,22 +1,39 @@
 import { useState, useEffect } from 'react';
 import { GetOrderTimesByDates } from '../../lib/api/order';
+import useHideCustomerName from '../../hooks/useHideCustomerName';
 
 const OrderPage = () => {
-	const [orderDate, setOrderDate] = useState();
-	const [orderTime, setOrderTime] = useState();
+	const [reservations, setReservations] = useState([]);
+	const [orderTimes, setOrderTimes] = useState([]);
 
 	useEffect(() => {
 		GetOrderTimesByDates()
-			.then(res => {
-				setOrderDate(res);
-				setOrderTime(res);
+			.then(data => {
+				setOrderTimes(data.availabilityByTime);
+				setReservations(data.reservationList);
 			})
-			.catch(err => console.log(err));
+			.catch(err => console.error('Error fetching order times:', err));
 	}, []);
+
 	return (
 		<>
-			<div>{orderDate}</div>
-			<div>{orderTime}</div>
+			<h2>기다리지 말고 현장에서 바로 픽업!</h2>
+			<ul>
+				{orderTimes.map((t, index) => (
+					<li key={index}>
+						Time slot {index}: {t === 1 ? '예약 가능' : '예약 불가'}
+					</li>
+				))}
+			</ul>
+			<h2>벗들의 Pick Up</h2>
+			<ul>
+				{reservations.map((r, index) => (
+					<li key={index}>
+						Date: {r.date}, Time: {r.time}, Customer:
+						{useHideCustomerName(r.customerName)}
+					</li>
+				))}
+			</ul>
 		</>
 	);
 };
