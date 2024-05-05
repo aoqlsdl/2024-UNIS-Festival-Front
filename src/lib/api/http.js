5f87abe7c0148633cc17220c1e9c492595f6fdde
aoqlsdl <hyeeev@gmail.com>
2024-05-06 03:03:03 +0900

    🔧fix: http 요청시 header 설정 변경

--- src/lib/api/http.js
+++ src/lib/api/http.js
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 export const http = axios.create({
 	baseURL: import.meta.env.VITE_APP_API_URL,
-	contentType: 'application/json; charset=utf-8;',
+	headers: {
+		'Content-Type': 'application/json; charset=utf-8',
+	},
 });
