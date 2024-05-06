12c9b589b79bc64a13a378ae151add5f488d42d2
aoqlsdl <hyeeev@gmail.com>
2024-05-07 02:49:28 +0900

    💬chore: 오타 수정

--- src/components/Stars/Stars.styles.js
+++ src/components/Stars/Stars.styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const Star = styled.div`
-	display: 'flex';
-	alignitems: 'center';
+	display: flex;
+	align-items: center;
 `;
--- src/components/StarsDisplay/StarsDisplay.styles.js
+++ src/components/StarsDisplay/StarsDisplay.styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const Star = styled.div`
-	display: 'flex';
-	align-items: 'center';
+	display: flex;
+	align-items: center;
+	margin-top: 1.13rem;
+	margin-bottom: 0.44rem;
 `;
