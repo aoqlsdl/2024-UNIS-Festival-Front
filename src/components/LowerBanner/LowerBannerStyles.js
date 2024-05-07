23708d373807b449e8f35b325e1edc623c8ce3f7
aoqlsdl <hyeeev@gmail.com>
2024-05-07 19:29:08 +0900

    🌈ui: 하단배너 슬라이드

--- src/components/LowerBanner/LowerBannerStyles.js
+++ src/components/LowerBanner/LowerBannerStyles.js
@@ -4,6 +4,15 @@ export const Container = styled.div`
 	width: 100vw;
 	min-width: 23.438rem;
 	max-width: 49.125rem;
-	height: 300px;
-	background-color: #dedede;
+	height: fit-content;
+	display: flex; // 이미지들을 가로로 배열
+	overflow: hidden; // 슬라이드 외의 이미지 숨김
+`;
+
+export const Img = styled.img`
+	flex: 0 0 100%; // 각 이미지가 컨테이너의 100% 너비를 차지
+	min-width: 23.438rem;
+	height: fit-content;
+	transition: transform 0.5s ease-in-out; // 부드러운 트랜지션 효과
+	margin: 0 auto;
 `;
