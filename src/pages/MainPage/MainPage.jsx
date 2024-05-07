f913c003b427a051e582d699bd87751ceb3bcfe2
aoqlsdl <hyeeev@gmail.com>
2024-05-07 17:17:43 +0900

    🔀merge: 메인페이지 병합

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -17,37 +17,6 @@ const MainPage = () => {
 		navigate('/reviews');
 	};
 
-	return (
-		<>
-			<Helmet>
-				<title>유니스 주막</title>
-			</Helmet>
-			<UpperBanner />
-			<s.Contents>
-				<s.Title>환영하오...</s.Title>
-				<MainButton title={'예약 주문'} desc={'기다리지 말고 픽업하세요!'} />
-				<MainButton title={'이벤트'} desc={'참여만 해도 반값할인'} />
-				<MainButton title={'오시는 길'} desc={'부스 위치 안내도'} />
-			</s.Contents>
-			<s.Contents className="review">
-				<s.Title>벗들의 생생한 후기</s.Title>
-				<BriefReviewBox />
-				<s.ReviewButton type="button" onClick={() => goToReview()}>
-					후기 남기고 기프티콘 받으러 가기
-				</s.ReviewButton>
-			</s.Contents>
-			<s.Contents>
-				<s.Title>어젯밤 구해주신 홍보물입니다</s.Title>
-			</s.Contents>
-			<LowerBanner />
-		</>
-	);
-	// useEffect(() => {
-	//     GetBriefReviewByTime().then(data => {
-	//         console.log(data);
-	//     });
-	// }, []);
-
 	const openPopup = link => {
 		window.open(link, '_blank', 'width=600,height=400');
 	};
@@ -61,44 +30,46 @@ const MainPage = () => {
 			<s.Contents>
 				<s.Title>환영하오...</s.Title>
 			</s.Contents>
-			<s.Contents>
-				<table style={{ width: '100vw' }}>
-					<tr>
-						<td>
-							<a href="/scr/pages/OrderListPage.jsx">
-								<MainButton
-									title={'예약 주문'}
-									desc={'기다리지 말고 픽업하세요!'}
-									imageSrc="/public/imgs/reservation.webp"
-								/>
-							</a>
-						</td>
-						<td>
-							<a href="https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%A6%AC%EB%85%B8">
-								<MainButton
-									title={'이벤트'}
-									desc={'참여만 해도 반값할인'}
-									imageSrc="/public/imgs/event.webp"
-								/>
-							</a>
-						</td>
-						<td>
+			<table style={{ width: '100vw' }}>
+				<tr>
+					<td>
+						<a href="/scr/pages/OrderListPage.jsx">
 							<MainButton
-								title={'오시는 길'}
-								desc={'부스 위치 안내도'}
-								imageSrc="/public/imgs/position.webp"
-								onClick={() =>
-									openPopup(
-										'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%A6%AC%EB%85%B8'
-									)
-								}
+								title={'예약 주문'}
+								desc={'기다리지 말고 픽업하세요!'}
+								imageSrc="/public/imgs/reservation.webp"
 							/>
-						</td>
-					</tr>
-				</table>
-			</s.Contents>
-			<s.Contents>
+						</a>
+					</td>
+					<td>
+						<a href="https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%A6%AC%EB%85%B8">
+							<MainButton
+								title={'이벤트'}
+								desc={'참여만 해도 반값할인'}
+								imageSrc="/public/imgs/event.webp"
+							/>
+						</a>
+					</td>
+					<td>
+						<MainButton
+							title={'오시는 길'}
+							desc={'부스 위치 안내도'}
+							imageSrc="/public/imgs/position.webp"
+							onClick={() =>
+								openPopup(
+									'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%A6%AC%EB%85%B8'
+								)
+							}
+						/>
+					</td>
+				</tr>
+			</table>
+			<s.Contents className="review">
 				<s.Title>벗들의 생생한 후기</s.Title>
+				<BriefReviewBox />
+				<s.ReviewButton type="button" onClick={() => goToReview()}>
+					후기 남기고 기프티콘 받으러 가기
+				</s.ReviewButton>
 			</s.Contents>
 			<s.Contents>
 				<s.Title>어젯밤 구해주신 홍보물입니다</s.Title>
