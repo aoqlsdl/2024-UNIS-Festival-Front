9c359b97d098a42c78522c533bfc7044f1a4d477
aoqlsdl <hyeeev@gmail.com>
2024-05-07 20:13:56 +0900

    ✨feat: 오픈 전 ui 설정

---
+++ src/components/BeforeOpen/BeforeOpen.styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	width: 100vw;
+	min-width: 23.438rem;
+	max-width: 49.125rem;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+export const Contents = styled.div`
+	height: fit-content;
+`;
+export const Title = styled.p`
+	font-size: 2rem;
+	margin: 0;
+`;
+
+export const Desc = styled.p`
+	font-size: 1.25rem;
+	margin: 0;
+`;
+
+export const Club = styled.p`
+	position: absolute;
+	bottom: 3rem;
+	font-size: 0.75rem;
+	color: #cdcdcd;
+`;
+
+export const Countdown = styled.div`
+	font-size: 1.25rem;
+	margin-top: 0.5rem;
+	color: var(--point);
+`;
--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -10,18 +10,36 @@ import ProtectedLayout from './ProtectedLayout';
 import AuthPage from './AuthPage/AuthPage';
 import useScrollToTop from '../hooks/useScrollToTop';
 import NotFoundPage from './NotFoundPage/NotFoundPage';
+import BeforeOpen from '../components/BeforeOpen/BeforeOpen';
 
 const RoutesComponent = () => {
 	useScrollToTop();
+
+	// 현재 날짜와 시간을 확인
+	const currentDate = new Date();
+	const openDate = new Date(
+		currentDate.getFullYear(),
+		currentDate.getMonth(),
+		8
+	); // 5월 8일을 설정 (월은 0부터 시작하므로 5월은 4)
+
+	// 5월 8일 0시 이전인지 확인
+	const isBeforeOpen = currentDate < openDate;
 	return (
 		<Routes>
 			{/* not found */}
 			<Route path="*" element={<NotFoundPage />} />
 
 			{/* 정상 경로 */}
-			<Route path="/" element={<Layout />}>
-				<Route index element={<MainPage />} />
-			</Route>
+			{isBeforeOpen ? (
+				<Route path="/">
+					<Route index element={<BeforeOpen />} />
+				</Route>
+			) : (
+				<Route path="/" element={<Layout />}>
+					<Route index element={<MainPage />} />
+				</Route>
+			)}
 			<Route path="/orders" element={<NavbarLayout title="예약 주문" />}>
 				<Route index element={<OrderListPage />} />
 				<Route path="order" element={<OrderPage />} /> /orders/1
