a4db5a50a66b8e575b32cc925dc8a98eb7c98782
aoqlsdl <hyeeev@gmail.com>
2024-05-08 01:44:14 +0900

    ♻️refactor: location대신 history 사용

--- src/lib/routeChangeTracker.js
+++ src/lib/routeChangeTracker.js
@@ -1,25 +1,31 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import ReactGA from 'react-ga';
 
-// pathname이 바뀔 때마다 Google Analytics에 페이지뷰 전송
 const RouteChangeTracker = () => {
-	const location = useLocation();
-
-	const [initialized, setInitialized] = useState(false);
+	const history = useHistory();
 
 	useEffect(() => {
 		if (!window.location.href.includes('localhost')) {
 			ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
 		}
-		setInitialized(true);
-	}, []);
 
-	useEffect(() => {
-		if (initialized) {
+		// 페이지뷰를 전송하는 함수를 정의
+		const sendPageView = location => {
 			ReactGA.pageview(location.pathname + location.search);
-		}
-	}, [initialized, location]);
+		};
+
+		// 초기 페이지 로딩 시 페이지뷰 전송
+		sendPageView(history.location);
+
+		// 라우트 변경 감지 시 페이지뷰 전송
+		const unlisten = history.listen(location => {
+			sendPageView(location);
+		});
+
+		// 컴포넌트 언마운트 시 리스너 해제
+		return unlisten;
+	}, [history]);
 
 	return null;
 };
