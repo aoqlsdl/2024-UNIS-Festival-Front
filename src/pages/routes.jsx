8b442fa268e7038f325568212d63b17eda40503f
aoqlsdl <hyeeev@gmail.com>
2024-05-07 20:25:46 +0900

    💬chore: qa를 위해 임시 오픈

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -31,7 +31,7 @@ const RoutesComponent = () => {
 			<Route path="*" element={<NotFoundPage />} />
 
 			{/* 정상 경로 */}
-			{isBeforeOpen ? (
+			{/* {isBeforeOpen ? (
 				<Route path="/">
 					<Route index element={<BeforeOpen />} />
 				</Route>
@@ -39,7 +39,10 @@ const RoutesComponent = () => {
 				<Route path="/" element={<Layout />}>
 					<Route index element={<MainPage />} />
 				</Route>
-			)}
+			)} */}
+			<Route path="/">
+				<Route index element={<BeforeOpen />} />
+			</Route>
 			<Route path="/orders" element={<NavbarLayout title="예약 주문" />}>
 				<Route index element={<OrderListPage />} />
 				<Route path="order" element={<OrderPage />} /> /orders/1
