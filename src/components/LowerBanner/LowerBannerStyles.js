54035f2e8808ed239f668e17de7085e9ecbc9a7d
aoqlsdl <hyeeev@gmail.com>
2024-05-07 22:48:45 +0900

    💬chore: height 늘어나는 문제 해결

--- src/components/LowerBanner/LowerBannerStyles.js
+++ src/components/LowerBanner/LowerBannerStyles.js
@@ -1,18 +1,17 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-	width: 100vw;
-	min-width: 23.438rem;
-	max-width: 49.125rem;
-	height: fit-content;
+	margin: 0 auto;
+	width: 21.56rem;
+	height: 8.06rem;
 	display: flex; // 이미지들을 가로로 배열
 	overflow: hidden; // 슬라이드 외의 이미지 숨김
 `;
 
 export const Img = styled.img`
 	flex: 0 0 100%; // 각 이미지가 컨테이너의 100% 너비를 차지
-	min-width: 23.438rem;
-	height: fit-content;
-	transition: transform 0.5s ease-in-out; // 부드러운 트랜지션 효과
+	width: 100%;
+	height: 100%;
+	transition: transform 0.5s ease-in-out;
 	margin: 0 auto;
 `;
