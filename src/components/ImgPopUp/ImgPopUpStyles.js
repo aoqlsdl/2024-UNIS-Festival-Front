3b07358fa93873a38ba57c50339e71f76fd5895d
aoqlsdl <hyeeev@gmail.com>
2024-05-07 19:30:35 +0900

    🌈ui: 자잘한 css 수정

--- src/components/ImgPopUp/ImgPopUpStyles.js
+++ src/components/ImgPopUp/ImgPopUpStyles.js
@@ -15,7 +15,7 @@ export const Background = styled.div`
 
 export const Img = styled.img`
 	width: 70%;
-	min-width: 23.438rem;
+	min-width: 20rem;
 	max-width: 49.125rem;
 	height: fit-content;
 	box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.5);
